e01e1cb528bf38a0fbd2cc35f74fc48873245057
ErikaTog <erika.togniolo@gmail.com>
2020-08-12 02:20:44 -0300

    incluindo variáveis do membro B

---
+++ src/js/variaveis.js
@@ -0,0 +1,51 @@
+//Suas variáveis abaixo
+
+// const linkAvatarMemberA = "";
+const linkAvatarMemberB = "https://avatars0.githubusercontent.com/u/56558061?s=460&u=a3f1ace0a0eef8b5f89a208a1083a2ced7938136&v=4";     
+
+// const linkGithubMemberA = "";
+const linkGithubMemberB = "https://github.com/ErikaTog";
+
+// const nameMemberA = "";
+const nameMemberB = "Erika";
+
+const comunnityName = "IDEV";
+
+const challengeTitle = "Variáveis";
+
+const technologiesYouLearned = ["Git", "GitHub", "JavaScript"];
+
+// const githubUserNameMemberA = "";
+const githubUserNameMemberB = "ErikaTog";
+
+const professionalObjetive = "Fullstack Developer";
+
+const yearYourObjective = "2022";
+
+const city = "São Bernardo do Campo";
+const day = 12;
+const month = "agosto";
+const year = 2020;
+const money = 6000;
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
